32fe540a4d12572ae383f86f05c3e0433de91487
kevho <kevincaizaguano2@gmail.com>
2022-05-30 17:35:04 -0500

    kevin pantalla_1

---
+++ app/src/main/java/fisei/uta/examen_caizaguanomaliza/Pantalla1Activity_KC_JM.java
@@ -0,0 +1,14 @@
+package fisei.uta.examen_caizaguanomaliza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla1Activity_KC_JM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla1_kc_jm);
+    }
+}
