e9e455931ea9a873ab27e0b58332d5a4785c6e7f
MalizaJessica <jessicamaliza19@gmail.com>
2022-05-30 17:37:29 -0500

    Pantalla2

---
+++ app/src/main/java/fisei/uta/examen_caizaguanomaliza/Pantalla2Activity_KC_JM.java
@@ -0,0 +1,14 @@
+package fisei.uta.examen_caizaguanomaliza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla2Activity_KC_JM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla2_kc_jm);
+    }
+}
