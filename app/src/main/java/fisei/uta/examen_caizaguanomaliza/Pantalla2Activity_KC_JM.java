31b138c18f11bb63023361f87fe754ebd542639d
kevho <kevincaizaguano2@gmail.com>
2022-05-30 18:35:48 -0500

    commit_k 18:35

--- app/src/main/java/fisei/uta/examen_caizaguanomaliza/Pantalla2Activity_KC_JM.java
+++ app/src/main/java/fisei/uta/examen_caizaguanomaliza/Pantalla2Activity_KC_JM.java
@@ -3,12 +3,53 @@ package fisei.uta.examen_caizaguanomaliza;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pantalla2Activity_KC_JM extends AppCompatActivity {
+    EditText editTextnumero ;
+    int contador =0;
+    List<String> lstDatos = new ArrayList<String>();
+    ListView listViewDatos;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2_kc_jm);
+
+        editTextnumero = findViewById(R.id.editTextNumero);
+        listViewDatos = findViewById(R.id.listViewDatos);
+
+    }
+
+    public void  ingresar (View view){
+
+        String numero = editTextnumero.getText().toString();
+        lstDatos.add(numero);
+        contador++;
+
+        ArrayAdapter<String> adapter  = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,lstDatos);
+        //asociar los datos
+        listViewDatos.setAdapter(adapter);
+    }
+
+    public List<String> cargarDatosListView() {
+
+        List<String> list = new ArrayList<String>();
+
+        for (int i =0; i<=22 ; i++) {
+            //String item = lstDatos[i];
+            ///list.add(item);
+        }
+
+        return  list;
     }
 }
